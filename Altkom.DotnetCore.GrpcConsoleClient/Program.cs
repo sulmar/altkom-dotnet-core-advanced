a842fa1ac678cea70e93d9dc7e8ea98a289b338f
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2020-10-06 10:32:08 +0200

    gRPC

---
+++ Altkom.DotnetCore.GrpcConsoleClient/Program.cs
@@ -0,0 +1,44 @@
+﻿using Altkom.DotnetCore.GrpcService;
+using Bogus;
+using Grpc.Net.Client;
+using System;
+using System.Threading.Tasks;
+
+namespace Altkom.DotnetCore.GrpcConsoleClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello gRPC Client!");
+
+            var locations = new Faker<AddLocationRequest>()
+                .RuleFor(p => p.Name, f => f.Lorem.Word())
+                .RuleFor(p => p.Latitude, f => f.Random.Float(-90f, 90f))
+                .RuleFor(p => p.Longitude, f => f.Random.Float(-90f, 90f))
+                .RuleFor(p => p.Speed, f => f.Random.Int(0, 140))
+                .RuleFor(p => p.Direction, f => f.Random.Float(0, 1))
+                .GenerateForever();
+
+            const string url = "https://localhost:5001";
+
+            GrpcChannel channel = GrpcChannel.ForAddress(url);
+            var client = new TrackingService.TrackingServiceClient(channel);
+
+            foreach (var location in locations)
+            {
+                await client.AddLocationAsync(location);
+
+                Console.WriteLine($"Send {location.Name} {location.Latitude} {location.Longitude} {location.Speed}");
+
+                await Task.Delay(TimeSpan.FromSeconds(0.01));
+            }
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
---
+++ Altkom.DotnetCore.GrpcService/Services/MyTrackingService.cs
@@ -0,0 +1,28 @@
+﻿using Grpc.Core;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Altkom.DotnetCore.GrpcService.Services
+{
+    public class MyTrackingService : TrackingService.TrackingServiceBase
+    {
+        private readonly ILogger<MyTrackingService> logger;
+
+        public MyTrackingService(ILogger<MyTrackingService> logger)
+        {
+            this.logger = logger;
+        }
+
+        public override Task<AddLocationResponse> AddLocation(AddLocationRequest request, ServerCallContext context)
+        {
+            logger.LogInformation($"{request.Name} {request.Latitude} {request.Longitude} {request.Speed}");
+
+            var response = new AddLocationResponse { IsConfirmed = true };
+
+            return Task.FromResult(response);
+        }
+    }
+}
